433a262d894ccdf4f80b4320f35aca7485e8fefe
bruno.onuki <bruno.onuki@LCET1115.cwbdis.pucpr.br>
2016-04-06 19:09:42 -0300

    produtos

---
+++ 04-Produtos/src/main/java/br/com/brunoonuki/controllers/ProdutoController.java
@@ -0,0 +1,12 @@
+package br.com.brunoonuki.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProdutoController {
+	@RequestMapping("/produtos")
+	public String todos() {
+		return "produtos";
+	}
+}
